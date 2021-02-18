8bb798d1c021ae9700b74ea3c76306ff5b07296e
Jacob Halsey <jacob@jhalsey.com>
2021-02-18 21:38:58 +0000

    relayout

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn example(code: &[u8]) -> CsResult<()> {
 fn main() {
     println!("Hello, world!");
 
-    let path = PathBuf::from("../programs/basic/basic.elf");
+    let path = PathBuf::from("programs/basic.elf");
     let file = match elf::File::open_path(&path) {
         Ok(f) => f,
         Err(e) => panic!("Error: {:?}", e),
